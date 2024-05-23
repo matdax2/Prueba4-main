c5376ce376758fa922898f06ecb125bbbdb73fcb
Matias Torterolo <matitorte1998@gmail.com>
2024-05-23 01:17:38 -0300

    casi pronto

--- proyectiño/PrimerApp/models.py
+++ proyectiño/PrimerApp/models.py
@@ -1,24 +1,44 @@
 from django.db import models
 
 class Curso(models.Model):
+
     curso = models.CharField(max_length=100)
+
     grupo = models.IntegerField()
+
     def __str__(self):
+
         return f"{self.curso}, Grupo: {self.grupo}"
 
+
+
 class Profesor(models.Model):
+
     nombre = models.CharField(max_length=100)
+
     apellido = models.CharField(max_length=100)
+
     email = models.EmailField(max_length=100)
+
     profesion = models.CharField(max_length=100)
+
     def __str__(self):
+
         return f"{self.nombre}, {self.apellido}"
 
+
+
+
 class Estudiante(models.Model):
+
     nombre = models.CharField(max_length=50)
+
     apellido = models.CharField(max_length=50)
+
     email = models.EmailField()
+
     def __str__(self):
+        
         return f"{self.nombre}, {self.apellido}"
     
     
